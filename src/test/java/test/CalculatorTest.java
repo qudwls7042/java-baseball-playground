c03466b4852bb910d43eb06dc71bf47e02e6985c
shkim <qudwls7042@gmail.com>
2021-10-30 15:44:19 +0900

    테스트코드 연습

---
+++ src/test/java/test/CalculatorTest.java
@@ -0,0 +1,48 @@
+package test;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.beans.Transient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator cal;
+
+    @BeforeEach
+    public void setUp() {
+        cal = new Calculator();
+    }
+
+    @Test
+    public void add() {
+        assertEquals(7, cal.add(3, 4));
+    }
+
+    @Test
+    public void subtract() {
+        assertEquals(3, cal.subtract(6, 3));
+    }
+
+    @Test
+    public void multiply() {
+        assertEquals(6, cal.multiply(2, 3));
+    }
+
+    @Test
+    public void divide() {
+        assertEquals(2, cal.divide(8, 4));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        cal = null;
+    }
+}
